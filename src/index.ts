1f8797d0f3889c94b992aa652a348b2f81f0140f
koheing <koheing.0111@gmail.com>
2022-07-16 19:50:27 +0900

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export * from './derives'
 export * from './observify'
 export * from './subjectify'
-export * from './types'
 export * from './models'
+export type {
+  Options,
+  Unsubscriber,
+  Subscriber,
+  Value,
+  ValueOrUpdator,
+} from './types'
